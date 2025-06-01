07202728ce90a23a00ab08512e63f788d111bdcf
kachi69366 <atkinsonbee@gmail.com>
2025-06-01 16:47:12 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+serial.redirect(
+SerialPin.P0,
+SerialPin.P1,
+BaudRate.BaudRate115200
+)
+serial.setRxBufferSize(32)
+let Math_question = Math.randomBoolean()
+music.play(music.stringPlayable("B A G A G F A C5 ", 120), music.PlaybackMode.UntilDone)
+basic.forever(function () {
+	
+})
